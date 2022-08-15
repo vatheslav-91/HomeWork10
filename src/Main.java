1fc542ce8e1ef31f40a001a15802afc1bc2f3772
Вячеслав <vatheslav-91@mail.ru>
2022-08-15 21:11:01 +0500

    ДЗ Обьекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("name1", "surname1");
+        Author author2 = new Author("name2", "surname2");
+
+        Book book1 = new Book("Title1", 2020, author1);
+        Book book2 = new Book("Title2", 2022, author2);
+        System.out.println("Автор: " + author1.getFullName());
+        System.out.println("Автор: " + author2.getFullName());
+
+        System.out.println("Название книги: " + book1.getName() + ", год издания книги: " + book1.getYear() + ",автор книги: " + book1.getAuthor().getFullName());
+        System.out.println("Название книги: " + book2.getName() + ", год издания книги: " + book2.getYear() + ",автор книги: " + book2.getAuthor().getFullName());
+
+
     }
 }
